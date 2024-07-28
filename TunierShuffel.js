463815aaa72f2ce26962c069ba009af5ca1dca6f
DerMixer <mickihde9@gmail.com>
2024-07-28 16:26:36 +0200

    sync

--- TunierShuffel.js
+++ TunierShuffel.js
@@ -1,6 +1,12 @@
-var Player = ["player1", "player2", "player3", "player4", "player5", "player6", "player7", "player8", "player9", "player10", "player11", "player12", "player13", "player14", "player15", "player16", "player17", "player18", "player19", "player20"];
-var PlayerCopy = [];
+var InputGroup = 'Gruppe A'
 var UsedPairs = [];
+var UsedSinglePairs = [];
+var GroupA = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
+var GroupB = ["14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27"];
+var MaxRoundLength = Math.floor((GroupA.length + GroupB.length) / 2 );
+var SearchCount = 0;
+var ACopy = [];
+var BCopy = [];
 var FullPairs = {
     'Runde:1': [],
     'Runde:2': [],
@@ -34,14 +40,19 @@ var TournamentPairs = {
 var Tierlist = {};
 var PlacementHolder = [];
 
-for (var i = 0; i < Player.length; i++) {
-    Tierlist[Player[i]] = 0
-}
-
 const prompt = require("prompt-sync")({ sigint: true });
 var shuffle = require('shuffle-array')
 
+function shuffleGroups() {
+    shuffle(ACopy)
+    shuffle(BCopy)
+}
+
 function ManegeTierlist(rounds) {
+    for (var i = 0; i < GroupA.length; i++) {
+        Tierlist[GroupA[i]] = 0 ;
+        Tierlist[GroupB[i]] = 0 ;
+    }
     if ( FullPairsCopy[`Runde:${rounds}`][0] == 'kein übriges Team') {
         return
     } else {
@@ -98,71 +109,89 @@ function MakeFullRounds() { //5
             FullPairs[`Runde:${rounds}`].shift()
             var Second = FullPairs[`Runde:${rounds}`][0]
             FullPairs[`Runde:${rounds}`].shift()
-            if (Second === undefined) {
-                Second = 'kein übriges Team';
-            }
             TournamentPairs[`Runde:${rounds}`].push(`${First} gegen ${Second}`)
         }
     }
     console.log(TournamentPairs)
+    console.log(MaxRoundLength, 'MaxRoundLength')
 }
 
-function RefillCopy() {//3
-    for (let i = 0; i < Player.length; i++) {
-        PlayerCopy.push(Player[i])
-        shuffle(PlayerCopy)
+function RefillCopys() {//3
+    for (var i = 0; i < GroupA.length; i++) {
+        if (!ACopy.includes(GroupA[i])) {
+            ACopy.push(GroupA[i]);
+        }
+    }
+    for (var i2 = 0; i2 < GroupB.length; i2++) {
+        if (!BCopy.includes(GroupB[i2])) {
+            BCopy.push(GroupB[i2]);
+        }
     }
 }
 
-function MakePairs(PlayerCopy,rounds) { //4
-    while (PlayerCopy.length > 0) {
-        var First = PlayerCopy[0];
-        PlayerCopy.shift(First);
-        shuffle(PlayerCopy)
-        var Second = PlayerCopy[0];
-        PlayerCopy.shift(Second);
-        shuffle(PlayerCopy)
-        if (UsedPairs.includes(`${First}${Second}`) === true || UsedPairs.includes(`${First}${Second}`) === true) {
-            PlayerCopy.push(First,Second)
-            shuffle(PlayerCopy)
-            if (PlayerCopy.length === 2) {
-                PlayerCopy.shift()
-                PlayerCopy.shift()
-            }
-        } else {
-            if( Second === undefined) {
-                Second = 'Kein Spielpartner mehr übrig'
-            }
-            else {
-                UsedPairs.push(`${First}${Second}`,`${Second}${First}`)
-            }
+function MakePairs(rounds) { //4
+    while (ACopy.length > 0 && BCopy.length > 0) {
+        var First = ACopy[0];
+        ACopy.shift();
+        var Second = BCopy[0];
+        BCopy.shift();
+        console.log(ACopy,'Acopy')
+        console.log(BCopy,'BCopy')
+        console.log(First, Second,'First Second')
+        if (UsedPairs.includes(`${First}${Second}`) === true || UsedPairs.includes(`${First}${Second}`) === true || UsedSinglePairs.includes(First) === true || UsedSinglePairs.includes(Second) === true) {
+            ACopy.push(First);
+            BCopy.push(Second);
+            shuffleGroups()
+            SearchCount += 1; 
+        } 
+        else {
+            UsedSinglePairs.push(`${First}`)
+            UsedSinglePairs.push(`${Second}`)
+            UsedPairs.push(`${First}${Second}`,`${Second}${First}`)
             FullPairs[`Runde:${rounds}`].push(`${First} + ${Second}`);
             FullPairsCopy[`Runde:${rounds}`].push(`${First} + ${Second}`)
-            shuffle(PlayerCopy)
+            SearchCount = 0;
         }
+        if ( FullPairs[`Runde:${rounds}`].length === MaxRoundLength ) {
+            return
+        }
+        if (SearchCount > 10000) {
+            return
+        }
+        RefillCopys()
     }
 }
 
 function HandleRounds() { //2
     for (let rounds = 1; rounds < 9; rounds++) {
-        RefillCopy()//3
-        shuffle(PlayerCopy)
-        MakePairs(PlayerCopy,rounds)//4
+        RefillCopys()//3
+        console.log('start')
+        MakePairs(rounds)//4
+        UsedSinglePairs = [];
+        console.log('end')
     }
     MakeFullRounds()//5
     MenageGameResults()
 }
 
 function GetInput() { //1
-    var Input = prompt('Gibt einen Teilnehmer ein (Wenn sie fertig sind geben sie "fertig" ein): ');
+    var Input = prompt(`Gibt einen Teilnehmer für ${InputGroup} ein (Wenn du fertig sind geben sie "fertig" ein): `);
+    if (Input == 'weiter') {
+        InputGroup = 'Gruppe B'
+        GetInput()
+    }
     if (Input == 'fertig') {
         HandleRounds();
         return
-    }
-    else {
-        Player.push(Input)
-        PlayerCopy.push(Input)
-        Tierlist[Input]
+    } else {
+        if (InputGroup == 'GruppeA') {
+            GroupA.push(Input)
+            Tierlist[Input]
+        }
+        if (InputGroup == 'GruppeB') {
+            GroupB.push(Input)
+            Tierlist[Input]
+        }
         GetInput()
     }
 }
